7a74cee194fbbb8433e5f043976baa87a6a471b6
nikolalov3 <nikolalovexo@gmail.com>
2025-12-14 20:28:06 +0100

    fix: Remove problematic user.profile assignment causing indentation error

--- basket_bot_backend/main.py
+++ basket_bot_backend/main.py
@@ -384,7 +384,6 @@ profile_data: ProfileUpdate, wallet_address: str = Depends(get_current_user), db
                 phone=profile_data.phone
             )
             db.add(profile)
-                user.profile = profile  # Set backref relationship
         
         await db.commit()
         await db.refresh(profile)
